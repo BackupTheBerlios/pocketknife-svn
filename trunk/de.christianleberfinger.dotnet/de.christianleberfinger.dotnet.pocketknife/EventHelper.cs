24adc10523bb343b4ce2f116eeea020b884fb32d
krizleebear <krizleebear@d3c4c337-602b-0410-8691-ddfa89b052a4>
2007-10-12 07:45:06 +0000

    EventHelper added. It was very much inspired by Juval Löwy's fantastic book '.NET components'.

---
+++ trunk/de.christianleberfinger.dotnet/de.christianleberfinger.dotnet.pocketknife/EventHelper.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace de.christianleberfinger.dotnet.pocketknife
+{
+    //using MyGenericHandler = EventHandler<string, int>;
+
+    public class EventHelper
+    {
+        private delegate void TestEventHandler(string a, int b);
+        private event TestEventHandler TestEvent;
+
+        /// <summary>
+        /// just for comparison
+        /// </summary>
+        private void basicInvoke()
+        {
+            TestEventHandler temp = TestEvent;
+            if (temp != null)
+            {
+                temp(null, 0);
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Unsafe means: the given arguments aren't checked for type safety (as they are objects)
+        /// </summary>
+        /// <param name="delegateToInvoke"></param>
+        /// <param name="args"></param>
+        public static void invokeUnsafe(Delegate delegateToInvoke, params object[] args)
+        {
+            if (delegateToInvoke == null)
+                return;
+
+            Delegate[] delegates = delegateToInvoke.GetInvocationList();
+            foreach (Delegate del in delegates)
+            {
+                try
+                {
+                    del.DynamicInvoke(args);
+                }
+                catch(Exception ex) 
+                {
+                    Debug.WriteLine(ex.Message);
+                };
+            }
+        }
+    }
+}
